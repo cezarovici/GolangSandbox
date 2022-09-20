970baaef860621fe486c1e4b5e7d4239b27a4064
cezar <njroffic@gmail.com>
2022-09-20 12:11:32 +0300

    parsing the files and packages too

--- 03_ProjectCreator1/main.go
+++ 03_ProjectCreator1/main.go
@@ -8,8 +8,6 @@ import (
 
 type entry struct {
 	folderInfo string
-	files      []string
-	pack       string
 	indent     int
 }
 
@@ -34,20 +32,39 @@ func readFile(filePath string) ([]string, error) {
 }
 
 func typeofFile(fileName string) string {
-	byteSlice := []byte(fileName)
 
-	for _, elem := range byteSlice {
-		switch elem {
-		case '!':
-			return "path"
-		case '#':
-			return "package"
-		case '.':
-			return "file"
-		}
+	if strings.Contains(fileName, "!") {
+		return "path"
+	}
+
+	if strings.Contains(fileName, ".") {
+		return "file"
+	}
+
+	if strings.Contains(fileName, "#") {
+		return "pack"
 	}
 
 	return "folder"
+}
+
+func lineParser(line, pack string) []string {
+	var res []string
+
+	files := strings.Split(line, " ")
+
+	for _, file := range files {
+		fileTrimmed := strings.TrimLeft(file, " ")
+		if fileTrimmed != "" {
+			res = append(res, isTestFile(pack, fileTrimmed))
+		}
+	}
+
+	return res
+}
+
+func getPackage(line string) string {
+	return line[2:]
 }
 
 func isTestFile(isPackage, line string) string {
@@ -81,8 +98,28 @@ func parse(entries []*entry) []string {
 
 	var stackFolders stack
 	var stackIndents stack
+	var stackPackages stack
 
 	for ix, entry := range entries {
+
+		if typeofFile(entry.folderInfo) == "pack" {
+			stackPackages.push(getPackage(entry.folderInfo))
+
+			continue
+		}
+
+		if typeofFile(entry.folderInfo) == "file" {
+			pack := stackPackages.front()
+			files := lineParser(entry.folderInfo, pack)
+
+			for _, file := range files {
+				line := stackFolders.String() + "/" + file
+				res = append(res, "touch "+line)
+			}
+
+			continue
+		}
+
 		if ix == 0 {
 			stackFolders.push(entry.folderInfo)
 			stackIndents.push(0)
@@ -95,6 +132,7 @@ func parse(entries []*entry) []string {
 		if entry.indent > stackIndents.peek().(int) {
 			stackFolders.push(entry.folderInfo)
 			stackIndents.push(entry.indent)
+			stackPackages.push("")
 
 			res = append(res, "mkdir "+stackFolders.String())
 
@@ -105,6 +143,7 @@ func parse(entries []*entry) []string {
 			stackFolders.pop()
 			stackFolders.push(entry.folderInfo)
 			stackIndents.push(entry.indent)
+			stackPackages.push("")
 
 			res = append(res, "mkdir "+stackFolders.String())
 
@@ -114,6 +153,7 @@ func parse(entries []*entry) []string {
 		for entry.indent < stackIndents.peek().(int) && len(stackIndents) > 1 {
 			if entry.indent == stackIndents.peek().(int) {
 				stackFolders.pop()
+				stackPackages.pop()
 
 				break
 			}
