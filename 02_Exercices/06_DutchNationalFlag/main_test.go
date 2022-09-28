5aff0e3a98365e6c92db3cad11bfebc663fc4b9b
cezar <njroffic@gmail.com>
2022-09-28 14:25:23 +0300

    more tests

--- 02_Exercices/06_DutchNationalFlag/main_test.go
+++ 02_Exercices/06_DutchNationalFlag/main_test.go
@@ -15,6 +15,11 @@ func TestSortSwapping(t *testing.T) {
 		{"short", []int{2, 1, 0}, []int{0, 1, 2}},
 		{"medium", []int{2, 0, 2, 1, 2, 0}, []int{0, 0, 1, 2, 2, 2}},
 		{"long", []int{2, 2, 2, 0, 0, 0, 1, 1, 1}, []int{0, 0, 0, 1, 1, 1, 2, 2, 2}},
+		{"equal2", []int{2, 2, 2}, []int{2, 2, 2}},
+		{"equal1", []int{1, 1, 1}, []int{1, 1, 1}},
+		{"nil", []int{}, []int{}},
+		{"increasing", []int{0, 1, 2}, []int{0, 1, 2}},
+		{"decreasing", []int{2, 1, 0}, []int{0, 1, 2}},
 	}
 
 	for _, tc := range testCases {
